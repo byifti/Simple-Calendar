01bc6002133de01bd5c32f574df13acfbde8db6a
Ifti <284367245+byifti@users.noreply.github.com>
2026-08-27 22:45:23 +0600

    Attempting to change months and render dynamically

--- main.js
+++ main.js
@@ -1,25 +1,105 @@
 // FROM HTML DOM
 const monthName = document.getElementById("monthName");
-const yearNum = document.getElementById("yearNum");
-let datesContainer = document.getElementById("datesContainer");
+const yearNumHeader = document.getElementById("yearNumHeader");
+const datesContainer = document.getElementById("datesContainer");
+const previousMonthBtn = document.getElementById("previousMonthBtn")
+const nextMonthBtn = document.getElementById("nextMonthBtn")
+
 // END
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const currentDate = new Date();
+let prevNextClicks = 0;
 
-let currentMonthIndex = currentDate.getMonth();
-let currentYearNum = currentDate.getFullYear();
+renderPresent();
 
-renderSpacingBeforeFirstDate()
-renderDates()
-
-// function renderHeader() {.textContent stuff below}
-monthName.textContent = months[currentMonthIndex];
-yearNum.textContent = currentYearNum;
-
-function renderDates()
+function renderPresent()
 {
-   let totalDays = getTotalDaysOfMonth(currentYearNum, currentMonthIndex);
+   const presentFullDate = new Date();
+   const presentMonthIndex = presentFullDate.getMonth();
+   const presentYearNum = presentFullDate.getFullYear();
+   const presentDate = presentFullDate.getDate();
+
+   renderHeader(presentYearNum, presentMonthIndex)
+   renderSpacingBeforeFirstDate(presentYearNum, presentMonthIndex)
+   renderDates(presentYearNum, presentMonthIndex)
+
+}
+
+function renderPreviousMonth()
+{
+   prevNextClicks++
+   const prevMonthFullDate = new Date()
+   prevMonthFullDate.setMonth(prevMonthFullDate.getMonth() - prevNextClicks);
+   let prevMonthIndex = prevMonthFullDate.getMonth();
+   let prevYearNum = prevMonthFullDate.getFullYear();
+
+   renderHeader(prevYearNum, prevMonthIndex)
+/* renderSpacingBeforeFirstDate(prevYearNum, prevMonthIndex)
+   renderDates(prevYearNum, prevMonthIndex) */
+
+   console.log(prevNextClicks)
+}
+function renderNextMonth()
+{
+   prevNextClicks++
+   const nextMonthFullDate = new Date()
+   nextMonthFullDate.setMonth(nextMonthFullDate.getMonth() + prevNextClicks);
+   let nextMonthIndex = nextMonthFullDate.getMonth();
+   let nextYearNum = nextMonthFullDate.getFullYear();
+
+   renderHeader(nextYearNum, nextMonthIndex)
+   console.log(prevNextClicks)
+}
+
+/* function renderPreviousMonth()
+{
+   const presentFullDate = new Date()
+   const prevMonth = presentFullDate.setMonth(presentFullDate.getMonth() - 1);
+   console.log(prevMonth)
+} */ // FIGURE OUT WHY DOES THIS NOT WORK???? IT GIVES WEIRD BIG NUMBER
+
+previousMonthBtn.addEventListener("click", renderPreviousMonth)
+nextMonthBtn.addEventListener("click", renderNextMonth)
+
+
+function getTotalDaysOfMonth(yearNum, monthIndex, date=0)
+{
+   yearNum = monthIndex + 1;
+   let totalDays = new Date(yearNum, monthIndex, date).getDate()
+   return totalDays
+}
+
+function renderHeader(yearNum, monthIndex) 
+{
+   monthName.textContent = months[monthIndex];
+   yearNumHeader.textContent = yearNum;
+} 
+
+function renderSpacingBeforeFirstDate(yearNum, monthIndex)
+{
+
+   let firstDayOfMonth = new Date(yearNum, monthIndex, 1).getDay();
+   let spacesBeforeFirstDay = firstDayOfMonth;
+
+   function renderSpace()
+   {
+
+      let spacingCell = document.createElement(`div`);
+      spacingCell.classList.add(`spacingCell`);
+      datesContainer.append(spacingCell)
+
+   }
+
+   for(let i = 0; i<spacesBeforeFirstDay; i++)
+   {
+      renderSpace()
+   }
+}
+
+function renderDates(yearNum, monthIndex)
+{
+
+   let totalDays = getTotalDaysOfMonth(yearNum, monthIndex);
 
    function renderCell(date)
    {
@@ -40,30 +120,33 @@ function renderDates()
 
 }
 
-function renderSpacingBeforeFirstDate()
+
+function clearOldDates()
 {
 
-   let currentFirstDay = new Date(currentYearNum, currentMonthIndex, 1).getDay();
-   let spacesBeforeFirstDay = currentFirstDay;
-
-   function renderSpace()
+   let cells = datesContainer.querySelectorAll(`.dateCell`);
+   cells.forEach(removeCells)
+   function removeCells(cell)
    {
-
-      let spacingCell = document.createElement(`div`);
-      spacingCell.classList.add(`spacingCell`);
-      datesContainer.append(spacingCell)
-
+      datesContainer.remove(cell)
    }
 
-   for(let i = 0; i<spacesBeforeFirstDay; i++)
-   {
-      renderSpace()
-   }
 }
 
-function getTotalDaysOfMonth(currentYear, currentMonth, date=0)
+function clearOldSpacing()
 {
-   currentMonth = currentMonth + 1;
-   let totalDays = new Date(currentYear, currentMonth, date).getDate()
-   return totalDays
+   let spacingCells = datesContainer.querySelectorAll(`spacingCell`)
+   spacingCells.forEach(removeSpacing) // queryselectorall works
+   
+   function removeSpacing(spacingCell)
+   {
+      datesContainer.remove(spacingCell)
+   }
+
 }
+
+
+/*
+The whole calender needs to know what state its in currently. 
+Pressing those buttons change the index to previous or the next 
+index. */
